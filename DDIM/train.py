bb3007767d6a77f8b43bb342eda566726de4e68e
nwaka013 <nwaka013@umn.edu>
2025-11-26 11:57:50 -0600

    more additions

--- DDIM/train.py
+++ DDIM/train.py
@@ -30,10 +30,14 @@ def train(config):
         start_epoch = cp["start_epoch"] + 1
 
     for epoch in range(start_epoch, config["epochs"] + 1):
+        print(f"Starting Epoch {epoch}/{config['epochs']}")
         loss = train_one_epoch(trainer, loader, optimizer, device, epoch)
+        
+        print(f"Epoch {epoch} finished. Avg Loss: {loss:.6f}")
         model_checkpoint.step(loss, model=model.state_dict(), config=config,
                               optimizer=optimizer.state_dict(), start_epoch=epoch,
                               model_checkpoint=model_checkpoint.state_dict())
+
 
 
 if __name__ == "__main__":
